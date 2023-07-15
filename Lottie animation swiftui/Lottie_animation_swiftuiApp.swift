b8da8157d3b42ff518c1c0ceaca740e1c6fb1219
piyushXP0067 <102165523+piyush3813@users.noreply.github.com>
2023-07-15 11:47:28 +0530

    anime app

---
+++ Lottie animation swiftui/Lottie_animation_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lottie_animation_swiftuiApp.swift
+//  Lottie animation swiftui
+//
+//  Created by Piyush Mishra on 15/07/23
+//
+
+import SwiftUI
+
+@main
+struct Lottie_animation_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
